9457fb58b9cb9a8d765ddb19403d23aead4e3f64
f <urka94@bk.ru>
2021-08-21 16:05:18 +0300

    feat: add fuctions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (typeof array == "undefined" || ((typeof array) != "undefined" && array.length == 0)) {
+    return 0;
+  }
+  let newArr = JSON.parse(JSON.stringify(array));
+  for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i] < newArr[i + 1]) {
+          let buf = newArr[i];
+          newArr[i + 1] = buf;
+          newArr[i] = newArr[i + 1]
+      }
+  }
+  return newArr[newArr.length - 1];
 }
-
 exports.max = function max (array) {
-  return 0;
+    if (typeof array == "undefined" || ((typeof array) != "undefined" && array.length == 0)) {
+        return 0;
+    }
+  let newArr = JSON.parse(JSON.stringify(array));
+  for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i] > newArr[i+1]) {
+          let buf = newArr[i];
+          newArr[i + 1] = buf;
+          newArr[i] = newArr[i + 1]
+        }
+  }
+  return newArr[newArr.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array == "undefined" || ((typeof array) != "undefined" && array.length == 0)) {
+        return 0;
+    }
+  let newArr = JSON.parse(JSON.stringify(array));
+  let sum =0;
+  for (let i = 0; i < newArr.length; i++) {
+    sum =sum + newArr[i];
+  }
+  return sum/newArr.length;
 }
